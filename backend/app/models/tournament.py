d0d104afccfdc5e39c496660837b540f7973d752
lisandru <alexandre.culioli@gmail.com>
2026-01-27 17:34:09 +0100

    ticket 12

--- backend/app/models/tournament.py
+++ backend/app/models/tournament.py
@@ -2,7 +2,7 @@ import uuid
 from sqlalchemy import Column, String, Date, Boolean, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from app.db.base import Base
-from app.services.tournament_closure import finish_tournament
+
 
 class Tournament(Base):
     __tablename__ = "tournaments"
